de54c16cf44bd5f3ad469e8249efff5b5cd9e3b3
Suraj N Raikar <144624284+SurajRaikar007@users.noreply.github.com>
2024-02-04 12:38:27 +0530

    second commit
    second change added string added

--- Test.java
+++ Test.java
@@ -3,9 +3,10 @@ class Test
 	public static void main(String[] args)
 	{
 		int num = 7;
+		String name = "7";
 		System.out.println("Hello This is First Program");
 
-		if(num==7)
+		if(num==7 && name.equals("7"))
 		{
 			System.out.println(num+" !! Thala for a Reason and Bole Jo Koyal.....");
 		}

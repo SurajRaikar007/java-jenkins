dc1decb3b207bbaf9f0b331b5235af0e2ae4d3ee
Suraj N Raikar <144624284+SurajRaikar007@users.noreply.github.com>
2024-02-04 13:18:19 +0530

    bike class added

---
+++ Bike.java
@@ -0,0 +1,22 @@
+public class Bike 
+{
+     int id;
+     String name;
+     int cc;
+    public Bike(int id, String name, int cc) 
+    {
+        this.id = id;
+        this.name = name;
+        this.cc = cc;
+    }
+  
+
+
+
+    public String toString() {
+        return "Bike [id=" + id + ", name=" + name + ", cc=" + cc + "]";
+    }
+
+  
+
+}
--- Test.java
+++ Test.java
@@ -2,6 +2,8 @@ class Test
 {
 	public static void main(String[] args)
 	{
+
+
 		int num = 7;
 		String name = "7";
 		System.out.println("Hello This is First Program");
@@ -19,6 +21,16 @@ class Test
 		{
 			System.out.println(" Sorry , No Thala for You ");	
 		}
+
+		System.out.println("The Bike.class has been added");
+
+		System.out.println();
+		Bike b = new Bike(1, "Yamaha RX 100", 98);
+		Bike b1 = new Bike(2, "KTM Duke 390 ", 373); 
+		Bike b2 = new Bike(3, "RAJDOOT 350 ", 349); 
+		System.out.println(b);
+		System.out.println(b1);
+		System.out.println(b2);
 
 
 

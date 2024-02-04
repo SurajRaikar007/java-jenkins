368bd047f45b61bbc1f3d9aa35d8f57664d68614
Suraj N Raikar <144624284+SurajRaikar007@users.noreply.github.com>
2024-02-04 12:26:32 +0530

    Initial commit

---
+++ Test.java
@@ -0,0 +1,22 @@
+class Test
+{
+	public static void main(String[] args)
+	{
+		int num = 7;
+		System.out.println("Hello This is First Program");
+
+		if(num==7)
+		{
+			System.out.println(num+" !! Thala for a Reason ");
+		}
+		else
+		{
+			System.out.println(" Sorry , No Thala for You ");	
+		}
+
+
+
+	}
+
+
+}
